80a4c54743ba720d6eeb54ef718ad941c49b8271
Chris Hatch <starforgedcode@gmail.com>
2019-10-11 08:16:44 -0500

    This makes more sense, I think.

--- setup-dir.sh
+++ setup-dir.sh
@@ -5,8 +5,8 @@ cd ~/$1
 mkdir .git
 cd .git
 git init --bare
-echo '#!/bin/sh
-GIT_WORK_TREE=/home/ubuntu/'"$1"'
+echo "#!/bin/sh
+GIT_WORK_TREE=/home/ubuntu/ $1
 export GIT_WORK_TREE
-git checkout -f' > hooks/post-receive
+git checkout -f" > hooks/post-receive
 chmod +x hooks/post-receive
