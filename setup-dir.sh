9d0c5faf4dce9a6edf80b21a14864f79085b5bed
Chris Hatch <starforgedcode@gmail.com>
2019-10-11 08:06:09 -0500

    Variables are only expanded in double quoted strings.

--- setup-dir.sh
+++ setup-dir.sh
@@ -6,7 +6,7 @@ mkdir .git
 cd .git
 git init --bare
 echo '#!/bin/sh
-GIT_WORK_TREE=/home/ubuntu/$1
+GIT_WORK_TREE=/home/ubuntu/'"$1"'
 export GIT_WORK_TREE
 git checkout -f' > hooks/post-receive
 chmod +x hooks/post-receive
